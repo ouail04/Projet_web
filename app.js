3cd67ccfdd8da99d79fe52969f4e5df27793b586
ouail04 <ouailzouina01@gmail.com>
2025-04-01 17:50:52 +0200

    l'ajout de rout creat account

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 const app = express();
 const port = 3000;
 const clientController = require('./controllers/clientController');
 const anonymeController = require('./controllers/anonymeController');
 const commercantController = require('./controllers/commercantController')
+const registerController = require('./controllers/registerController')
 
 // Configuration EJS
 app.set('view engine', 'ejs');
@@ -14,7 +16,12 @@ app.use('/static', express.static(path.join(__dirname, 'static'))); // Pointe ve
 app.use(express.urlencoded({ extended: true })); // Middleware
 
 
-
+app.use(session({
+    secret: '123456ABCDEF', // Une clé secrète pour signer les sessions
+    resave: false,          // Ne pas sauver la session si elle n'a pas été modifiée
+    saveUninitialized: true, // Sauvegarder la session même si elle n'a pas été modifiée
+    cookie: { secure: false } 
+}));
 
 
 
@@ -33,6 +40,19 @@ app.get('/commercant', commercantController.showCommercantIndexPage);
 app.get('/mes-offres', commercantController.showCommercantOffres);
 app.get('/commercant-commandes', commercantController.showCommercantCommandes);
 app.get('/commercant-profil', commercantController.showCommercantProfil);
+
+//Route register
+app.get('/register', registerController.showRegisterPage);
+app.post('/commerce-register', registerController.showCommerceRegisterPage);
+app.post('/commerce-payment-register', registerController.showCommercePaymentRegisterPage);
+
+//Route login
+app.get('/login', registerController.showLoginPage);
+
+
+
+//fonctionnalite
+app.get('/create-account', registerController.createAccount);
 
 app.listen(port, () => {
     console.log(`Serveur démarré sur http://localhost:${port}`);
