98d9c137d59ba4ba650559122ed0d0af8453af71
ouail <ouailzouina01@gmail.com>
2025-04-04 18:25:47 +0200

    modification des fonctions pour telecharger les données dynamiquement

--- controllers/anonymeController.js
+++ controllers/anonymeController.js
@@ -1,4 +1,5 @@
-//const OfferModel = require('../models/OfferModel');
+const session = require('express-session');
+const offre = require('../models/offre');
 
 exports.showHomePage = async (req, res) => {
     const pages = [
@@ -6,19 +7,29 @@ exports.showHomePage = async (req, res) => {
         {titre : "Offres", lien : "/offres"},
         {titre : "Comment ça marche ?", lien : "/#comment-ca-marche"},
         {titre : "Contact", lien : "#contact"}];
-    const latestOffers = [1, 2, 3, 4, 5, 6]; 
-    const avis = [1,2,3] ;
-    css_files = ["anonyme/offres-section-index.css", "anonyme/hero-section.css", "anonyme/comment-ca-marche.css","anonyme/avis-index.css"] ;
 
-
-    res.render('anonyme/index', {
-        titre: 'Accueil',
-        pages,
-        css_files,
-        offers: latestOffers,
-        avis,
-        session: req.session
-    });
+    try{
+        const offres = await offre.searchOfferClient('', 'all', 'all', 'all', '', true) ; 
+        const css_files = [
+            "anonyme/avis-index.css", 
+            "anonyme/comment-ca-marche.css", 
+            "anonyme/header-anonyme.css", 
+            "anonyme/hero-section.css",
+            "anonyme/offres-section-index.css"
+        ];
+        res.render('anonyme/index', {
+            titre: 'Accueil',
+            pages,
+            css_files,
+            offers: offres,
+            avis: [1,2,3],
+            session: req.session
+        });
+    }
+    catch (error) {
+        console.error("Erreur lors de la recherche d'offres:", error);
+        res.status(500).render('erreur', { message: "Une erreur est survenue lors de la recherche." });
+    }
 };
 
 exports.showOffersPage = async (req, res) => {
@@ -28,19 +39,35 @@ exports.showOffersPage = async (req, res) => {
         {titre : "Comment ça marche ?", lien : "/#comment-ca-marche"},
         {titre : "Contact", lien : "#contact"}];
 
-    const offres = [1, 2, 3, 4, 5, 6]; 
-    const css_files = ["filtre-section-offres.css", "offres.css"] ;
-    const nb_pages = 3 ;
-    const current_page = 1 ;
-    
-    res.render('anonyme/offres', {
-        titre: 'Offres',
-        pages,
-        css_files,
-        offers: offres,
-        nb_pages,
-        current_page
-    });
+    try{
+        const offres = await offre.searchOfferClient('', 'all', 'all', 'all', '') ; 
+        const css_files = [
+            "anonyme/panier.css", 
+            "anonyme/filtre-section-offres.css", 
+            "anonyme/offres-section-index.css", 
+            "anonyme/offres.css"
+        ];
+        const nb_pages = 3 ;
+        const current_page = 1 ;
+        const avis = [1,2,3] ;
+        res.render('anonyme/offres', {
+            titre: 'Offres',
+            pages,
+            css_files,
+            offers: offres,
+            nb_pages,
+            current_page, 
+            nom_offre : '',
+            type : 'all',
+            sorted_experation : 'all',
+            sorted_prix: 'all',
+            ville : ''
+        });
+    }
+    catch (error) {
+        console.error("Erreur lors de la recherche d'offres:", error);
+        res.status(500).render('erreur', { message: "Une erreur est survenue lors de la recherche." });
+    }
 };
 
 exports.showHowItWorksSection = async (req, res) => {
